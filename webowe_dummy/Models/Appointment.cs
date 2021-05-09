b5c63bd6716396157f35310720c215a9c3c187b3
pbeat8 <61192446+pbeat8@users.noreply.github.com>
2021-05-09 22:05:54 +0200

    Dodaj pliki projektów.

---
+++ webowe_dummy/Models/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webowe_dummy.Models
+{
+    public class Appointment
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public int PhysiciansID { get; set; }
+        public int LocationID { get; set; }
+        public bool Booked { get; set; }
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+        public int PatientPhoneNumber { get; set; }
+        public string PatientEmail { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ webowe_dummy/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WeboweTwojDentysta.Models
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+    }
+}
